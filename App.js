ba3ae81a015f90d1a8f5b43c245614623d624fad
narayan jha <narayan@opoyi.com>
2024-01-01 21:00:17 +0530

    feat:(Laying the foundatioon)

--- App.js
+++ App.js
@@ -1,41 +1,58 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-/**
- *
- * nested
- *
- * <div id="parent">
-  <div id="child">
-    <h1>I'm h1 tag </h1>
+import ReactDom from "react-dom/client";
+
+// React.createElement => Object => HTMLElemet(render)
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Deev Dive React 🚀"
+);
+
+console.log(heading);
+
+// JSX -> it is like html and xml synatx
+
+//  like react element  is this a valid ajavscript ? your js enginge does
+// not understand JSx it understand ecmascrip es6
+
+// React element
+const jsxHeading = (
+  <h1 classname="head" tabIndex="5">
+    React Deep Dive
+  </h1>
+);
+// JSX (transpiled before it reaches the JS Engine) -> PARCEL (Its manage and provide responsibility to ) - BABEL
+
+// JSX => React.createElement => ReactElement-JS Object {babel responsibility } => HTMLElement(render)
+
+// React Component
+// Class Based Component
+// Functional Component = its a normal js function
+const Title = () => (
+  <h1 classname="head" tabIndex="5">
+    Title
+  </h1>
+);
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <Title></Title>
+    <Title />
+    {jsxHeading}
+    {/* // component composition  -> calling component inside component */}
+    <h1 classname="head" tabIndex="5">
+      React functional component Deep Dive
+    </h1>
   </div>
-</div>;
+);
 
- *
- *
- *ReactElement(Object) => HTML(Browser Understand)
- */
+console.log(jsxHeading);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm in h1 tag"),
-    React.createElement("h2", {}, "I'm in h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm in h1 tag"),
-    React.createElement("h2", {}, "I'm in h2 tag"),
-  ]),
-]);
+const root = ReactDom.createRoot(document.getElementById("root"));
+// root.render(heading); // how you render element
+root.render(<HeadingComponent />); // how you render Component
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello World from React!"
-// );
-// this {} is the place where you give attribute to tags
+console.log(root);
 
-//JSX
-
-console.log(parent); //object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+// JSX -> is a javascript synatx which make easire to craete react element
